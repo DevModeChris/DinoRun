3a3b4f49f73cfb9b645a117f17a462a54b3ef8bb
DevModeChris <cwalsh88@outlook.com>
2025-01-11 06:28:33 +0000

    Fix image assets missing from game build

--- vite/config.dev.js
+++ vite/config.dev.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     base: './',
@@ -16,4 +17,14 @@ export default defineConfig({
             },
         },
     },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: './src/assets',
+                    dest: './src/',
+                },
+            ],
+        }),
+    ],
 });
