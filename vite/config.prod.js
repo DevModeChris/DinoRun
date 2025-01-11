8b7cff32b42cd7325d2079efc08d59bed05736d0
DevModeChris <cwalsh88@outlook.com>
2025-01-11 05:07:40 +0000

    Remove terser from vite build

--- vite/config.prod.js
+++ vite/config.prod.js
@@ -16,15 +16,5 @@ export default defineConfig({
                 },
             },
         },
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                passes: 2,
-            },
-            mangle: true,
-            format: {
-                comments: false,
-            },
-        },
     },
 });
